db666b0df4b87db71e52b2b404bc20d8cffa9544
sheroine <42820740+sheroine@users.noreply.github.com>
2024-07-27 03:36:08 +0800

    Camera move and stop, trigger boxes
    Camera stays in bounds and will pan downwards to mush-room. Trigger boxes placed but awaits coding

---
+++ scene/entity/mushroom_trigger.gd
@@ -0,0 +1,17 @@
+extends Area2D
+@export var camera : Camera2D
+@export var marker : Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body : CharacterBody2D):
+	camera.move_to_mushroom(marker.position)
+	print("You found the secret Mush-room!")
--- scene/level/ScrollingCamera.gd
+++ scene/level/ScrollingCamera.gd
@@ -1,13 +1,47 @@
 extends Camera2D
 
 @export var current_level : Level
+@export var tilemap : TileMap
 var scroll_speed : int
+
+@onready var found_mushroom : bool = false
+var start_pos : Vector2
+var marker_pos : Vector2
+@onready var t : float = 0.0
 #@export var player : CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	scroll_speed = current_level.scroll_speed
+	set_camera_limits()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.x += scroll_speed * delta
+	
+	# moves to a y pos that sees the mush-room
+	# probably a dumb janky way to do this but this is what i can think of rn
+	if found_mushroom:
+		# takes 5s to interpolate to the target
+		t += delta / 5
+		position.y = lerpf(start_pos.y, marker_pos.y, t)
+		#to stop this bit from continuously running after it's done
+		#idk if it's necessary though
+		if t > 1:
+			found_mushroom = false
+
+func set_camera_limits():
+	#this reads how large the tile map is
+	#then sets the camera limit based on the size of the tile map
+	#limit makes it so the camera auto stops and will not go further than set limit
+	var map_limits = tilemap.get_used_rect()
+	var map_cellsize = tilemap.rendering_quadrant_size
+	limit_left = map_limits.position.x * map_cellsize
+	limit_right = map_limits.end.x * map_cellsize
+	limit_top = map_limits.position.y * map_cellsize
+	limit_bottom = map_limits.end.y * map_cellsize
+	
+func move_to_mushroom(marker : Vector2):
+	start_pos = position
+	marker_pos = marker
+	found_mushroom = true
