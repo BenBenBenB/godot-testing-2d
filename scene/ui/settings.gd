505e42e01d8a70eeddc480e69fcf6d47001586c4
Ben Burton <benjaminburton@gmail.com>
2024-07-29 01:29:18 -0400

    add settings menu functionality

---
+++ scene/ui/settings.gd
@@ -0,0 +1,35 @@
+extends Control
+class_name SettingsMenu
+
+signal close_settings()
+
+var music_bus = AudioServer.get_bus_index("Music")
+var sfx_bus = AudioServer.get_bus_index("SFX")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func activate() -> void:
+	show()
+	set_process(true)
+	set_process_input(true)
+	set_process_unhandled_input(true)
+
+func deactivate() -> void:
+	hide()
+	set_process(false)
+	set_process_input(false)
+	set_process_unhandled_input(false)
+
+func _on_music_slider_mouse_exited():
+	release_focus()
+	
+func _on_sfx_slider_mouse_exited():
+	release_focus()
+
+func _on_music_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(music_bus, linear_to_db(value))
+
+func _on_sfx_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(sfx_bus, linear_to_db(value))
