f0b89a2cbd62dddb6aeb823e3615a0c0741a1499
sheroine <42820740+sheroine@users.noreply.github.com>
2024-07-30 20:15:37 +0800

    making a spawner class

---
+++ spawner.gd
@@ -0,0 +1,15 @@
+class_name Spawner
+extends Marker2D
+
+@export var spawn : PackedScene
+@export var spawn_rect : Vector2 = Vector2(0.0, 0.0)
+@export var spawn_radius : float = 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
