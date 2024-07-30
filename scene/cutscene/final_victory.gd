028f104e031d08444c48ca69eae01ccebacbad3e
Ben Burton <benjaminburton@gmail.com>
2024-07-29 23:03:53 -0400

    add victory screen

--- scene/cutscene/final_victory.gd
+++ scene/cutscene/final_victory.gd
@@ -1,11 +1,23 @@
 extends Node2D
 
-@onready var player_top_down = $PlayerTopDown
-var final_size: float = 2500
-var mass_per_second: float = 300
+@onready var player_blackhole = $PlayerBlackhole
+@onready var earf = $Earf
+@onready var start_timer = $StartTimer
+var final_scale: float = 7.5
+var scale_per_second: float = 1.0
 
+
+func _ready():
+	player_blackhole.dead = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if player_top_down.mass < final_size:
-		player_top_down.consume_mass(delta * mass_per_second)
+	if not start_timer.is_stopped():
+		var deltaScale = 0.5 * delta
+		player_blackhole.scale += Vector2(deltaScale, deltaScale)
+		return
+	if player_blackhole.scale.x < final_scale:
+		var deltaScale = scale_per_second * delta
+		player_blackhole.scale += Vector2(deltaScale, deltaScale)
+	else:
+		earf.visible = false
