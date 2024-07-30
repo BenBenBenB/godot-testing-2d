2134bb7093d2b03d023f29c520ff62a41f8b65f7
Ben Burton <benjaminburton@gmail.com>
2024-07-29 23:13:57 -0400

    player start pos not hardcoded

--- scene/level/level_01.gd
+++ scene/level/level_01.gd
@@ -1,10 +1,12 @@
 extends Level
 
 @onready var player_blackhole: PlayerBlackhole = $PlayerBlackhole
+@onready var player_start = $Positions/PlayerStart
 
 func _ready() -> void:
 	super()
 	player_blackhole.stop_scroll.connect(stop_scroll)
+	player_blackhole.position = player_start.position
 
 func stop_scroll():
 	scroll_speed = 0.0
