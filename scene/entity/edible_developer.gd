311c75b8f31b7efb290e08fa51e87d3c8b982036
Ben Burton <benjaminburton@gmail.com>
2024-07-31 01:15:40 -0400

    animated credits sprites

--- scene/entity/edible_developer.gd
+++ scene/entity/edible_developer.gd
@@ -4,23 +4,19 @@ signal dev_eaten()
 
 @export var dev_name: String
 @export var title: String
-@export var sprite_source: String
 @export var mass: float = 100
 
-@onready var developer_sprite = $DeveloperSprite
 @onready var dev_name_label = $DevNameLabel
 @onready var title_label = $TitleLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var picture = load("res://assets/%s" % sprite_source)
-	developer_sprite.texture = picture
 	dev_name_label.text = dev_name
 	title_label.text = title
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 func _on_body_entered(body):
--- scene/level/credits.gd
+++ scene/level/credits.gd
@@ -1,6 +1,20 @@
 extends Level
 
-func _process(delta):
+@onready var sprite_ben = $BenB/AnimatedSprite2D
+@onready var sprite_cratylus = $Cratylus/AnimatedSprite2D
+@onready var sprite_sheroine = $Sheroine/AnimatedSprite2D
+@onready var sprite_tortex = $Tortex/AnimatedSprite2D
+@onready var sprite_magicsmoke = $Magicsmoke/AnimatedSprite2D
+@onready var background_music = $BackgroundMusic
+
+func _ready():
+	sprite_ben.play()
+	sprite_cratylus.play()
+	sprite_sheroine.play()
+	sprite_tortex.play()
+	sprite_magicsmoke.play()
+	background_music.play()
+func _process(_delta):
 	if Input.is_action_pressed("pause"):
 		go_to_menu()
 
