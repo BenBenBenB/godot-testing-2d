c1842d65e030bf6352f9541ef2b616ed36b940e5
Ben Burton <benjaminburton@gmail.com>
2024-07-31 02:39:20 -0400

    make volume persistent for lifetime of app

--- scene/main.gd
+++ scene/main.gd
@@ -4,6 +4,11 @@ extends Node
 
 @onready var _scene_2d = $Scene2D
 
+var music_bus = AudioServer.get_bus_index("Music")
+var sfx_bus = AudioServer.get_bus_index("SFX")
+
 func _ready() -> void:
 	LevelManager.main_scene = _scene_2d
 	LevelManager.levels = available_levels
+	AudioServer.set_bus_volume_db(music_bus, linear_to_db(0.2))
+	AudioServer.set_bus_volume_db(sfx_bus, linear_to_db(0.5))
--- scene/ui/settings.gd
+++ scene/ui/settings.gd
@@ -9,16 +9,14 @@ var sfx_bus = AudioServer.get_bus_index("SFX")
 @onready var music_slider = $NinePatchRect/VBoxContainer/GridContainer/MusicSlider
 @onready var sfx_slider = $NinePatchRect/VBoxContainer/GridContainer/SfxSlider
 @onready var sfx_player = $sfx
-
-func _ready():
-	AudioServer.set_bus_volume_db(music_bus, linear_to_db(music_slider.value))
-	AudioServer.set_bus_volume_db(sfx_bus, linear_to_db(sfx_slider.value))
-
+	
 func activate() -> void:
 	show()
 	set_process(true)
 	set_process_input(true)
 	set_process_unhandled_input(true)
+	music_slider.value = db_to_linear(AudioServer.get_bus_volume_db(music_bus))
+	sfx_slider.value = db_to_linear(AudioServer.get_bus_volume_db(sfx_bus))
 
 func deactivate() -> void:
 	hide()
@@ -34,15 +32,15 @@ func _on_sfx_slider_mouse_exited():
 
 func _on_music_slider_value_changed(value):
 	AudioServer.set_bus_volume_db(music_bus, linear_to_db(value))
-	sfx_player.play_press_sfx()
 
 func _on_sfx_slider_value_changed(value):
 	AudioServer.set_bus_volume_db(sfx_bus, linear_to_db(value))
-	sfx_player.play_press_sfx()
-	
-
 
 func _on_back_button_pressed():
 	emit_signal("close_settings")
+
+func _on_music_slider_drag_ended(value_changed):
 	sfx_player.play_press_sfx()
-	
+
+func _on_sfx_slider_drag_ended(value_changed):
+	sfx_player.play_press_sfx()
